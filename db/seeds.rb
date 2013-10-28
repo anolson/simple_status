c29dd4e5ad8523237c3a037c5d1338744cb2aeeb
Andrew Olson <aolson@geezeo.com>
2013-10-28 08:38:49 -0400

    Fixing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-SystemConfigs.create(name: "Acme", description: "Acme System Status")
+SystemConfig.create(name: "Acme", description: "Acme System Status")
 
 Status.create(current: true, state: "up", last_updated: Time.current)
 MessageCreation.create(body: "All systems go!")
